29b5f7751453a8568d55435e85578df88bb4f5f0
Chris Olstrom <chris@olstrom.com>
2016-10-10 13:12:25 -0700

    Add SelfPreservation module

---
+++ lib/ego/self_preservation.rb
@@ -0,0 +1,55 @@
+require_relative 'memory'
+require_relative 'spatial_awareness'
+
+module SelfPresevation
+  include Memory
+  include SpatialAwareness
+
+  MAX_HEALTH = 20
+  HEALING_RATE = 0.1
+
+  def safe?
+    adjacent(:enemy).empty? && !taking_ranged_damage?
+  end
+
+  def wounds
+    MAX_HEALTH - health
+  end
+
+  def taking_damage?
+    wounds > (remembered(:wounds) || 0)
+  end
+
+  def taking_ranged_damage?
+    taking_damage? && ([:walk!, :rest!].include? actions.last)
+  end
+
+  def wounded?
+    true unless wounds.zero?
+  end
+
+  def worth_resting?
+    wounds >= recoverable && look.find(&:enemy?)
+  end
+
+  def recoverable
+    MAX_HEALTH * HEALING_RATE
+  end
+
+  def recover
+    return unless wounded?
+    rest! if safe?
+    retreat if worth_resting? && !taking_ranged_damage?
+  end
+
+  def avoid_facing_walls
+    return unless adjacent(:wall).include? :forward
+    pivot! :backward
+  end
+
+  def archers
+    nearby(:enemy).select do |direction|
+      look(direction).reject(&:empty?).first.character == 'a'
+    end
+  end
+end
